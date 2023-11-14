ea8fe99bff8a23467e77a733299b974775f09fd9
WeiDarkest <zasoxg@gmail.com>
2023-11-14 17:33:45 +0300

    Fourth commit
    Drag & Drop finally

--- TechDesignTestProject_KirdanMaksim/Assets/Scripts/DragAndDrop/UIItem.cs
+++ TechDesignTestProject_KirdanMaksim/Assets/Scripts/DragAndDrop/UIItem.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    private CanvasGroup _canvasGroup;
     private Canvas _mainCanvas;
     private RectTransform _rectTransform;
 
@@ -12,21 +13,24 @@ public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHa
     {
         _rectTransform = GetComponent<RectTransform>();
         _mainCanvas = GetComponentInParent<Canvas>();
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         var slotTransform = _rectTransform.parent;
         slotTransform.SetAsLastSibling();
+        _canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        _rectTransform.anchoredPosition += eventData.delta / _mainCanvas.scaleFactor * 2;
+        _rectTransform.anchoredPosition += eventData.delta / _mainCanvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.localPosition = Vector3.zero;
+        _canvasGroup.blocksRaycasts = true;
     }
 }
--- TechDesignTestProject_KirdanMaksim/Assets/Scripts/DragAndDrop/UISlot.cs
+++ TechDesignTestProject_KirdanMaksim/Assets/Scripts/DragAndDrop/UISlot.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UISlot : MonoBehaviour
+public class UISlot : MonoBehaviour, IDropHandler
 {
-    
+    public void OnDrop(PointerEventData eventData)
+    {
+        var otherItemTransform = eventData.pointerDrag.transform;
+        otherItemTransform.SetParent(transform);
+        otherItemTransform.localPosition = Vector3.zero;
+    }
 }
